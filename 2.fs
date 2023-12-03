75fae980e59fa6a249589e0f369d3bdcf9b2deed
Oliver Gilan <github@olivergilan.com>
2023-12-03 12:44:49 -0800

    completed day 2

--- 2.fs
+++ 2.fs
@@ -15,23 +15,31 @@ module Two
     red <= redCount && green <= greenCount && blue <= blueCount
 
   let extractNum (str: string) = str |> Seq.filter Char.IsDigit |> String.Concat |> int
+  let extractColorCount (set: string) = 
+    set.Split(",") |> Seq.fold (fun (r, g, b) (c: string) -> 
+      if c.Contains("red") then
+        (extractNum c, g, b)
+      elif c.Contains("green") then
+        (r, extractNum c, b)
+      elif c.Contains("blue") then
+        (r, g, extractNum c)
+      else
+        (r, g, b)
+    ) (0, 0, 0)
 
-  let foldMaxColors (red, green, blue) (c: string) =
-    if c.Contains("red") then
-      (Math.Max(red, extractNum c), green, blue)
-    elif c.Contains("green") then
-      (red, Math.Max(green, extractNum c), blue)
-    elif c.Contains("blue") then
-      (red, green, Math.Max(blue, extractNum c))
-    else
-      (red, green, blue)
+  let foldMaxColors (red, green, blue) (set: string) =
+    let (r, g, b) = extractColorCount set
+    (Math.Max(red, r), Math.Max(green, g), Math.Max(blue, b))
 
-  let parseCubeGame (str: string) : CubeGame = 
-    let parseMaxSet (str: string) = str.Split(";") |> Array.fold foldMaxColors (0, 0, 0)
+  let parseCubeGame (game: string) : CubeGame = 
+    let parseMaxSet (gameSets: string) = gameSets.Split(";") |> Array.fold foldMaxColors (0, 0, 0)
     {
-      id = extractNum(str.Split(":")[0])
-      maxColors = parseMaxSet(str.Split(":")[1])
+      id = extractNum(game.Split(":")[0])
+      maxColors = parseMaxSet(game.Split(":")[1])
     }
   
   let sumValidGames games = games |> Seq.filter isValidGame |> Seq.sumBy (fun g -> g.id)
 
+  // Part 2
+  let setPower (r, g, b) = r * g * b
+  let minSetPowerSum (games: CubeGame seq) = games |> Seq.map (fun g -> g.maxColors) |> Seq.map setPower |> Seq.sum
--- Program.fs
+++ Program.fs
@@ -8,4 +8,7 @@
     let calibration = calculateCalibration document
     printfn "Calibration: %d" calibration
     let games = readLines "./inputs/2a.txt" |> Seq.map parseCubeGame
+    let sum = sumValidGames games
+    printfn "Sum valid games: %d" sum
+    printfn "Sum min set power: %d" (minSetPowerSum games)
     0
